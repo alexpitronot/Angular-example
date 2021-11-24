adf99cfb3d713f1912dcabcf2686445fb3036d0d
Alex Gorbachov <alexpitronot@gmail.com>
2021-11-24 08:39:19 +0200

    Update employees.component.ts

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import { Employee } from '../employee';
-import { Employees } from '../list-employees';
+import { Employee } from '../employee';
+import { EMPLOYEES } from '../list-employees';
 
 
 @Component({
@@ -10,7 +10,7 @@ import { Employees } from '../list-employees';
 })
 export class EmployeesComponent implements OnInit {
 
-  employee = Employees;
+  employees = EMPLOYEES;
 
   constructor() { }
 
