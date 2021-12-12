ff152010d4584a8baf1c66e510776bc073b282c8
Alex Gorbachov <alexpitronot@gmail.com>
2021-12-12 17:44:42 +0200

    12.12.

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Employee } from './employee';
 import { EMPLOYEES } from './list-employees';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,8 @@ export class EmployeeService {
 
   getEmployees(): Observable<Employee[]> {
     const employees = of(EMPLOYEES);
+    this.messageService.add('Employee Service message: all employees were updated!');
     return employees;
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
